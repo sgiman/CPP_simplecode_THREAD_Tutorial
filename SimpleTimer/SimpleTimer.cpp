b8a9813129e37121a08dd7627f28e0a59c465446
Sergey Gasanov <sgimancs@gmail.com>
2023-09-12 01:59:15 +0300

    Update SimpleTimer.cpp

--- SimpleTimer/SimpleTimer.cpp
+++ SimpleTimer/SimpleTimer.cpp
@@ -23,12 +23,12 @@ class SimpleTimer
 public:
 	SimpleTimer()	// Конструрктор
 	{
-		start = std::chrono::high_resolution_clock::now();					// текущее время старта 
+		start = std::chrono::high_resolution_clock::now();			// текущее время старта 
 	}
 	~SimpleTimer()	// Деструктор
 	{
-		end = std::chrono::high_resolution_clock::now();					// время конца (перед удалением объекта)
-		std::chrono::duration<float> duration = end - start;				// продолжительность
+		end = std::chrono::high_resolution_clock::now();			// время конца (перед удалением объекта)
+		std::chrono::duration<float> duration = end - start;			// продолжительность
 		cout << "\n**** Duration: " << duration.count() << "s\n" << endl;
 	}
 
